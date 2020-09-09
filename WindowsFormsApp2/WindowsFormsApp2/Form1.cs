baf7ad957bcaf2096a85b1103c865b710a3e07cc
Bodgan Humenyk <dxbogdan4@gmail.com>
2020-09-09 15:46:16 +0300

    Initial Commit

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "";
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked) { label1.Text = "Yes"; }
+            else { label1.Text = "No"; }
+        }
+    }
+}
