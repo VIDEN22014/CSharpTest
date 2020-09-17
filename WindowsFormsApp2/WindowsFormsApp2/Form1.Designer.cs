5fc652d4c749049d020c72c135f038523337d2b7
Bodgan Humenyk <dxbogdan4@gmail.com>
2020-09-17 15:49:37 +0300

    level1 nearly added

--- WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -43,79 +43,98 @@
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.button10 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button1.Location = new System.Drawing.Point(155, 92);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 75);
             this.button1.TabIndex = 0;
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button2.Location = new System.Drawing.Point(236, 92);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 75);
             this.button2.TabIndex = 1;
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button3.Location = new System.Drawing.Point(317, 92);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 75);
             this.button3.TabIndex = 2;
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button4.Location = new System.Drawing.Point(155, 173);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 75);
             this.button4.TabIndex = 3;
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button5.Location = new System.Drawing.Point(236, 173);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 75);
             this.button5.TabIndex = 4;
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button6.Location = new System.Drawing.Point(317, 173);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(75, 75);
             this.button6.TabIndex = 5;
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button7.Location = new System.Drawing.Point(155, 254);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(75, 75);
             this.button7.TabIndex = 6;
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button8.Location = new System.Drawing.Point(236, 254);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(75, 75);
             this.button8.TabIndex = 7;
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button9.Location = new System.Drawing.Point(317, 254);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(75, 75);
             this.button9.TabIndex = 8;
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // label1
             // 
@@ -159,6 +178,7 @@
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Легкий";
             this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
             // 
             // radioButton2
             // 
@@ -170,6 +190,7 @@
             this.radioButton2.TabIndex = 13;
             this.radioButton2.Text = "Середній";
             this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
             // 
             // radioButton3
             // 
@@ -181,12 +202,25 @@
             this.radioButton3.TabIndex = 14;
             this.radioButton3.Text = "Важкий";
             this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button10.Location = new System.Drawing.Point(468, 254);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(118, 75);
+            this.button10.TabIndex = 15;
+            this.button10.Text = "Рестарт";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(999, 545);
+            this.Controls.Add(this.button10);
             this.Controls.Add(this.radioButton3);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
@@ -226,6 +260,7 @@
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button button10;
     }
 }
 
--- WindowsFormsApp2/WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -13,9 +13,227 @@ namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
     {
+        bool isPlayerTurn = true;
+        int[,] field = new int[3, 3];
+        int difficulty = 0;
         public Form1()
         {
             InitializeComponent();
+            label2.Text = "Ваш Хід";
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    field[i, j] = 0;
+                }
+            }
+
+        }
+        int isGameOver()
+        {
+            int sumHorizontal = 0, sumVertical = 0, sumDiagonal1 = 0, sumDiagonal2 = 0, sumTotal = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                sumHorizontal = 0;
+                sumVertical = 0;
+                sumDiagonal1 += field[i, i];
+                sumDiagonal2 += field[2 - i, i];
+                for (int j = 0; j < 3; j++)
+                {
+                    if (field[i, j] != 0)
+                    {
+                        sumTotal++;
+                    }
+                    sumHorizontal += field[i, j];
+                    sumVertical += field[j, i];
+                }
+                if (sumHorizontal == 3 || sumVertical == 3 || sumDiagonal1 == 3 || sumDiagonal2 == 3) { return 1; }
+                else if (sumHorizontal == 30 || sumVertical == 30 || sumDiagonal1 == 30 || sumDiagonal2 == 30) { return 2; }
+                else if (sumTotal == 9) { return 3; }
+            }
+            return 0;
+        }
+        bool isEmpty(int buttonIndex)
+        {
+            if (field[Convert.ToInt32(Math.Floor(buttonIndex / 3.0)), buttonIndex % 3] == 0)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+        int level1Turn()
+        {
+            int buttonIndex = -1;
+            int sumHorizontal = 0, sumVertical = 0, sumDiagonal1 = 0, sumDiagonal2 = 0;
+            int sumIndex = -1, iterationIndex = -1;
+            for (int i = 0; i < 3; i++)
+            {
+                
+                sumDiagonal1 += field[i, i];
+                sumDiagonal2 += field[2 - i, i];
+                sumHorizontal += field[i, 0] + field[i, 1] + field[i, 2];
+                sumVertical += field[0, i] + field[1, i] + field[2, i];
+                if (sumDiagonal1 == 2 || sumDiagonal1 == 20) {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (isEmpty(j * 4)) { return j * 4; }
+                    }
+                }
+                //if (sumDiagonal2 == 2 || sumDiagonal2 == 20) {
+                //    for (int j = 1; j < 4; j++)
+                //    {
+                //        if (isEmpty(j * 2)) { return j * 2; }
+                //    }
+                //}
+                if (sumHorizontal == 2 || sumHorizontal == 20) { sumIndex = 2; iterationIndex = i; }
+                if (sumVertical == 2 || sumVertical == 20) { sumIndex = 3; iterationIndex = i; }
+            }
+            return -1;
+        }
+        int level0Turn()
+        {
+            int buttonIndex = 0;
+            Random rnd = new Random();
+            do
+            {
+                buttonIndex = rnd.Next(0, 9);
+            } while (!isEmpty(buttonIndex));
+            return buttonIndex;
+        }
+        async Task BotTurn()
+        {
+            int buttonIndex = 0;
+            if (difficulty == 0)
+            {
+                buttonIndex = level0Turn();
+            }
+            else if (difficulty == 1)
+            {
+                buttonIndex = level1Turn();
+                if (buttonIndex == -1) { buttonIndex = level0Turn(); }
+            }
+            else if (difficulty == 2)
+            {
+
+            }
+            await Task.Delay(1000);
+            GetButton(buttonIndex).Text = "O";
+            field[Convert.ToInt32(Math.Floor(buttonIndex / 3.0)), buttonIndex % 3] = 10;
+            if (isGameOver() == 2) { label2.Text = "Ви Програли!!!"; }
+            else if (isGameOver() == 3) { label2.Text = "Нічия"; }
+            else
+            {
+                isPlayerTurn = true;
+                label2.Text = "Ваш Хід";
+            }
+
+        }
+        void Turn(int buttonIndex)
+        {
+            if (isGameOver() == 0 && isPlayerTurn && isEmpty(buttonIndex))
+            {
+                GetButton(buttonIndex).Text = "X";
+                field[Convert.ToInt32(Math.Floor(buttonIndex / 3.0)), buttonIndex % 3] = 1;
+                if (isGameOver() == 1) { label2.Text = "Ви Виграли!!!"; }
+                else if (isGameOver() == 3) { label2.Text = "Нічия"; }
+                else
+                {
+                    isPlayerTurn = false;
+                    label2.Text = "Хід Комп'ютера";
+                    BotTurn();
+                }
+            }
+
+        }
+
+        Button GetButton(int buttonIndex)
+        {
+            switch (buttonIndex)
+            {
+                case 0:
+                    return button1;
+                case 1:
+                    return button2;
+                case 2:
+                    return button3;
+                case 3:
+                    return button4;
+                case 4:
+                    return button5;
+                case 5:
+                    return button6;
+                case 6:
+                    return button7;
+                case 7:
+                    return button8;
+                case 8:
+                    return button9;
+            }
+            return button1;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Turn(0);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Turn(1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Turn(2);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Turn(3);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Turn(4);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Turn(5);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Turn(6);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Turn(7);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Turn(8);
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            difficulty = 0;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            difficulty = 1;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            difficulty = 2;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
         }
     }
 }
