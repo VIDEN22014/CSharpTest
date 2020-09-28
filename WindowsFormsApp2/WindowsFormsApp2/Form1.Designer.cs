d23e50c55882aacef23fcf8128e674aa3ba7de8c
Bodgan Humenyk <dxbogdan4@gmail.com>
2020-09-28 18:10:14 +0300

    Interpolation added

--- WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -28,57 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series17 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series18 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series19 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series20 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title5 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.label1 = new System.Windows.Forms.Label();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 70);
+            this.label1.Location = new System.Drawing.Point(35, 70);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(115, 16);
+            this.label1.Size = new System.Drawing.Size(152, 16);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Зсув Фаз 0 - 180°";
+            this.label1.Text = "К-сть точок Синусоїди";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            this.chart1.Location = new System.Drawing.Point(295, 12);
+            chartArea5.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea5);
+            legend5.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom;
+            legend5.Name = "Legend1";
+            this.chart1.Legends.Add(legend5);
+            this.chart1.Location = new System.Drawing.Point(327, 3);
             this.chart1.Name = "chart1";
-            series1.BorderWidth = 2;
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series1.Color = System.Drawing.Color.SeaGreen;
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(550, 550);
+            series17.BorderWidth = 8;
+            series17.ChartArea = "ChartArea1";
+            series17.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series17.Color = System.Drawing.Color.SeaGreen;
+            series17.Legend = "Legend1";
+            series17.LegendText = "Синусоїда";
+            series17.Name = "Series1";
+            series18.BorderWidth = 4;
+            series18.ChartArea = "ChartArea1";
+            series18.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series18.Color = System.Drawing.Color.Red;
+            series18.Legend = "Legend1";
+            series18.LegendText = "Лінійна";
+            series18.Name = "Series2";
+            series19.BorderWidth = 4;
+            series19.ChartArea = "ChartArea1";
+            series19.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series19.Color = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            series19.Legend = "Legend1";
+            series19.LegendText = "Ермітова";
+            series19.Name = "Series3";
+            series20.BorderWidth = 5;
+            series20.ChartArea = "ChartArea1";
+            series20.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series20.Color = System.Drawing.Color.Blue;
+            series20.Legend = "Legend1";
+            series20.LegendText = "Дані";
+            series20.Name = "Series4";
+            this.chart1.Series.Add(series17);
+            this.chart1.Series.Add(series18);
+            this.chart1.Series.Add(series19);
+            this.chart1.Series.Add(series20);
+            this.chart1.Size = new System.Drawing.Size(758, 669);
             this.chart1.TabIndex = 2;
             this.chart1.Text = "chart1";
-            title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            title1.Name = "Title1";
-            title1.Text = "Фігура Ліссажу";
-            this.chart1.Titles.Add(title1);
+            title5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            title5.Name = "Title1";
+            title5.Text = "Інтерполяція";
+            this.chart1.Titles.Add(title5);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(158, 67);
+            this.textBox1.Location = new System.Drawing.Point(204, 64);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 4;
@@ -87,70 +120,23 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(52, 98);
+            this.label2.Location = new System.Drawing.Point(41, 104);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(87, 16);
+            this.label2.Size = new System.Drawing.Size(146, 16);
             this.label2.TabIndex = 5;
-            this.label2.Text = "Амплітуда X";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(52, 126);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(88, 16);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Амплітуда Y";
+            this.label2.Text = "К-сть точок в Лінійній";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(158, 95);
+            this.textBox2.Location = new System.Drawing.Point(204, 101);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 22);
             this.textBox2.TabIndex = 7;
             // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(158, 123);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 22);
-            this.textBox3.TabIndex = 8;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(158, 151);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 22);
-            this.textBox4.TabIndex = 9;
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(158, 179);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 22);
-            this.textBox5.TabIndex = 10;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(66, 154);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(73, 16);
-            this.label4.TabIndex = 11;
-            this.label4.Text = "Частота X";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(66, 182);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(74, 16);
-            this.label5.TabIndex = 12;
-            this.label5.Text = "Частота Y";
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(97, 227);
+            this.button1.Location = new System.Drawing.Point(143, 278);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(161, 60);
             this.button1.TabIndex = 14;
@@ -158,19 +144,87 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(204, 174);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(97, 20);
+            this.checkBox1.TabIndex = 15;
+            this.checkBox1.Text = "Синусоїда";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(204, 200);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(77, 20);
+            this.checkBox2.TabIndex = 16;
+            this.checkBox2.Text = "Лінійна";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.Location = new System.Drawing.Point(204, 226);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(92, 20);
+            this.checkBox3.TabIndex = 17;
+            this.checkBox3.Text = "Ермітова";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Checked = true;
+            this.checkBox4.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox4.Location = new System.Drawing.Point(204, 252);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(58, 20);
+            this.checkBox4.TabIndex = 18;
+            this.checkBox4.Text = "Дані";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(204, 136);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 20;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 16);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "К-сть точок на інтервал";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(999, 545);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
+            this.ClientSize = new System.Drawing.Size(1173, 699);
             this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
@@ -188,14 +242,14 @@
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- WindowsFormsApp2/WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -8,15 +8,113 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
     {
+        static int dataSize;
+        static int interpolatedDataSize;
+        static int interpolatedHermiteDataSize;
+
         public Form1()
         {
             InitializeComponent();
         }
+        struct point
+        {
+            public double posx { get; set; }
+            public double posy { get; set; }
+        }
+        abstract class СInterpolation
+        {
+            protected point[] data { get; set; }
+            protected point[] interpolatedData { get; set; }
+            public СInterpolation(int dataSize, int interpolatedDataSize)
+            {
+
+                data = new point[dataSize];
+                interpolatedData = new point[interpolatedDataSize];
+                for (int i = 0; i < dataSize; i++)
+                {
+                    data[i].posx = i * (Math.PI * 2 / (dataSize - 1));
+                    data[i].posy = Math.Sin(data[i].posx);
+                }
+                data[dataSize - 1].posy = 0;
+                for (int i = 0; i < interpolatedDataSize; i++)
+                {
+                    interpolatedData[i].posx = i * (Math.PI * 2 / (interpolatedDataSize - 1));
+                }
+            }
+
+        }
+
+        class LineInterpolation : СInterpolation
+        {
+            public LineInterpolation(int dataSize, int interpolatedDataSize) : base(dataSize, interpolatedDataSize) { }
+            public point[] Interpolate()
+            {
+                double x, y;
+                for (int i = 1; i < interpolatedDataSize; i++)
+                {
+                    for (int j = 0; j < dataSize - 1; j++)
+                    {
+                        if (interpolatedData[i].posx > data[j].posx && interpolatedData[i].posx <= data[j + 1].posx)
+                        {
+                            x = data[j + 1].posx - data[j].posx;
+                            y = data[j + 1].posy - data[j].posy;
+                            interpolatedData[i].posy = data[j].posy + ((interpolatedData[i].posx - data[j].posx) * y / x);
+                        }
+                    }
+                }
+                return interpolatedData;
+            }
+        }
+
+        class HermiteInterpolation : СInterpolation
+        {
+            point[] dxData { get; set; }
+            public HermiteInterpolation(int dataSize, int interpolatedHermiteDataSize) : base(dataSize, ((interpolatedHermiteDataSize+1) * (dataSize-1)) + 1)
+            {
+                dxData = new point[dataSize];
+                for (int i = 0; i < dataSize; i++)
+                {
+                    dxData[i].posx = data[i].posx;
+                    dxData[i].posy = Math.Cos(dxData[i].posx);
+                }
+            }
+            public point[] Interpolate()
+            {
+                int interval = 0;
+                int dataIndex = 0;
+                double Ax, Bx, dxAx, dxBx, Ay, By, dxAy, dxBy, t = 0,i=0;
+                while (interval < (dataSize - 1))
+                {
+                    Ax = data[interval].posx;
+                    Ay = data[interval].posy;
+                    Bx = data[interval + 1].posx;
+                    By = data[interval + 1].posy;
+                    dxAx = dxData[interval].posx;
+                    dxAy = dxData[interval].posy;
+                    dxBx = dxData[interval + 1].posx;
+                    dxBy = dxData[interval + 1].posy;
+                    interpolatedData[dataIndex].posx = Ax + dxAx * t + (-3 * Ax + 3 * Bx - 2 * dxAx - dxBx) * Math.Pow(t, 2) + (2 * Ax - 2 * Bx + dxAx + dxBx) * Math.Pow(t, 3);
+                    interpolatedData[dataIndex].posy = Ay + dxAy * t + (-3 * Ay + 3 * By - 2 * dxAy - dxBy) * Math.Pow(t, 2) + (2 * Ay - 2 * By + dxAy + dxBy) * Math.Pow(t, 3);
+                    dataIndex++;
+                    t = t + 1 / (interpolatedHermiteDataSize + 1.0);
+                    i++;
+                    if (i > interpolatedHermiteDataSize)
+                    {
+                        t = 0;
+                        i = 0;
+                        interval++;
+                    }
+                }
+                return interpolatedData;
+            }
+        }
+
 
         private void label1_Click(object sender, EventArgs e)
         {
@@ -27,28 +125,91 @@ namespace WindowsFormsApp2
         {
 
         }
-        public double textBoxToDouble(TextBox sender)
+        public int textBoxToInt(TextBox sender)
         {
             if (sender.Text == "") { return 0; }
-            return Convert.ToDouble(sender.Text);
+            return Convert.ToInt32(sender.Text);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            double delta = 0, A = 0, B = 0, a = 0, b = 0, ratio = 0, x = 0, y = 0;
-            delta = textBoxToDouble(textBox1);
-            A = textBoxToDouble(textBox2);
-            B = textBoxToDouble(textBox3);
-            a = textBoxToDouble(textBox4);
-            b = textBoxToDouble(textBox5);
-            ratio = b / a;
-            chart1.Series[0].Points.Clear();
-            for (double t = 0; t < 50; t += 0.01)
+            if (textBoxToInt(textBox1) <= 1 || textBoxToInt(textBox2) <= 1||textBoxToInt(textBox3)<1)
             {
-                x = A * Math.Sin(t * 10 + delta * Math.PI / 180.0);
-                y = B * Math.Sin(t * 10 * ratio);
-                chart1.Series[0].Points.AddXY(x, y);
+                return;
             }
+            dataSize = textBoxToInt(textBox1);
+            interpolatedDataSize = textBoxToInt(textBox2);
+            interpolatedHermiteDataSize = textBoxToInt(textBox3);
+            LineInterpolation interpolationLinear = new LineInterpolation(dataSize, interpolatedDataSize);
+            HermiteInterpolation interpolationHermite = new HermiteInterpolation(dataSize, interpolatedHermiteDataSize);
+            chart1.Series[0].Points.Clear();
+            if (checkBox1.Checked)
+            {
+                double x, y;
+                for (int i = 0; i < 100; i++)
+                {
+                    x = i * (Math.PI * 2 / (99));
+                    y = Math.Sin(x);
+                    chart1.Series[0].Points.AddXY(x, y);
+                }
+            }
+            chart1.Series[1].Points.Clear();
+            if (checkBox2.Checked)
+            {
+                point[] linear = interpolationLinear.Interpolate();
+                for (int i = 0; i < interpolatedDataSize; i++)
+                {
+                    chart1.Series[1].Points.AddXY(linear[i].posx, linear[i].posy);
+                }
+
+
+            }
+            chart1.Series[2].Points.Clear();
+            if (checkBox3.Checked)
+            {
+
+                point[] hermite = interpolationHermite.Interpolate();
+                for (int i = 0; i < ((interpolatedHermiteDataSize+1) * (dataSize - 1)) + 1 ; i++)
+                {
+                    chart1.Series[2].Points.AddXY(hermite[i].posx, hermite[i].posy);
+                }
+            }
+            chart1.Series[3].Points.Clear();
+            if (checkBox4.Checked)
+            {
+                double x, y;
+                for (int i = 0; i < dataSize; i++)
+                {
+                    x = i * (Math.PI * 2 / (dataSize - 1));
+                    y = Math.Sin(x);
+                    chart1.Series[3].Points.AddXY(x, y);
+                }
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            chart1.Series[0].IsVisibleInLegend = checkBox1.Checked;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            chart1.Series[1].IsVisibleInLegend = checkBox2.Checked;
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            chart1.Series[2].IsVisibleInLegend = checkBox3.Checked;
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            chart1.Series[3].IsVisibleInLegend = checkBox4.Checked;
         }
     }
 }
