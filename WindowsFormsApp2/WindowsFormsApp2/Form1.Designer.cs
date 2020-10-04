aa9211565773d3afcd4796f110fcbcf6f241c1f6
Bodgan Humenyk <dxbogdan4@gmail.com>
2020-10-04 20:43:43 +0300

    2/3 Done

--- WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -28,39 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea10 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend10 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(319, 227);
+            this.button1.Location = new System.Drawing.Point(362, 224);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(114, 32);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Інтегрувати";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(197, 68);
+            this.label1.Location = new System.Drawing.Point(283, 65);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 16);
+            this.label1.Size = new System.Drawing.Size(73, 16);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "Ліва межа";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(299, 61);
+            this.textBox1.Location = new System.Drawing.Point(362, 62);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 2;
@@ -68,35 +77,124 @@
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(200, 193);
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(362, 146);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(104, 20);
+            this.radioButton1.Size = new System.Drawing.Size(91, 20);
             this.radioButton1.TabIndex = 3;
             this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.Text = "Функція 1";
             this.radioButton1.UseVisualStyleBackColor = true;
             // 
             // chart1
             // 
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
+            chartArea10.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea10);
+            legend10.Name = "Legend1";
+            this.chart1.Legends.Add(legend10);
             this.chart1.Location = new System.Drawing.Point(509, 43);
             this.chart1.Name = "chart1";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chart1.Series.Add(series2);
+            series10.ChartArea = "ChartArea1";
+            series10.Legend = "Legend1";
+            series10.Name = "Series1";
+            this.chart1.Series.Add(series10);
             this.chart1.Size = new System.Drawing.Size(300, 300);
             this.chart1.TabIndex = 4;
             this.chart1.Text = "chart1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(362, 90);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(362, 118);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(269, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Права межа";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(317, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Крок";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(362, 172);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(91, 20);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.Text = "Функція 2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(362, 198);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(91, 20);
+            this.radioButton3.TabIndex = 10;
+            this.radioButton3.Text = "Функція 3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(233, 346);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(278, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Площа Методом середніх прямокутників=";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(330, 374);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(181, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Площа Методом трапецій=";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(249, 202);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 16);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Крок";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(999, 545);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.textBox1);
@@ -117,6 +215,15 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- WindowsFormsApp2/WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -13,14 +13,92 @@ namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
     {
+        static double leftBorder, rightBorder, step;
+        static RadioButton[] radio = new RadioButton[3];
         public Form1()
         {
             InitializeComponent();
+            radio[0] = radioButton1;
+            radio[1] = radioButton2;
+            radio[2] = radioButton3;
         }
-
+        static double getY(double x)
+        {
+            if (radio[0].Checked == true)
+            {
+                return Math.Sin(x);
+            }
+            if (radio[1].Checked == true)
+            {
+                return Math.Sqrt(x) - 1;
+            }
+            return 0;
+        }
+        struct point
+        {
+            public double posx { get; set; }
+            public double posy { get; set; }
+        }
+        interface IIntegral
+        {
+            double Integrate();
+        }
+        class MidRectangleMethod : IIntegral
+        {
+            double s = 0, h = leftBorder + step / 2;
+            public MidRectangleMethod() { }
+            public double Integrate()
+            {
+                while (h < rightBorder)
+                {
+                    s += step * getY(h);
+                    h += step;
+                }
+                return s;
+            }
+        }
+        class TrapeziumMethod : IIntegral
+        {
+            double s = 0;
+            point[] data;
+            public TrapeziumMethod()
+            {
+                double x = leftBorder;
+                data = new point[Convert.ToInt32(Math.Ceiling((rightBorder - leftBorder) / step) + 1)];
+                for (int i = 0; x < rightBorder; i++)
+                {
+                    data[i].posx = x;
+                    data[i].posy = getY(x);
+                    x += step;
+                }
+                data[data.Length - 1].posx = rightBorder;
+                data[data.Length - 1].posy = getY(rightBorder);
+            }
+            public double Integrate()
+            {
+                s = (data[data.Length - 1].posy + data[0].posy) / 2;
+                for (int i = 1; i < data.Length - 1; i++)
+                {
+                    s += data[i].posy;
+                }
+                s *= step;
+                return s;
+            }
+        }
+        double TextBoxToDouble(TextBox textBox)
+        {
+            if (textBox.Text == "") { return 0; }
+            return Convert.ToDouble(textBox.Text);
+        }
         private void button1_Click(object sender, EventArgs e)
         {
-
+            leftBorder = TextBoxToDouble(textBox1);
+            rightBorder = TextBoxToDouble(textBox2);
+            step = TextBoxToDouble(textBox3);
+            MidRectangleMethod method1 = new MidRectangleMethod();
+            TrapeziumMethod method2 = new TrapeziumMethod();
+            label4.Text = "Площа Методом середніх прямокутників=" + Convert.ToString(method1.Integrate());
+            label5.Text = "Площа Методом трапецій=" + Convert.ToString(method2.Integrate());
         }
     }
 }
