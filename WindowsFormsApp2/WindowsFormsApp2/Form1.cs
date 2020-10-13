5d27ddcc9dcb66dd68996e2e7729d5a2b6e7cc03
Bodgan Humenyk <dxbogdan4@gmail.com>
2020-10-13 20:22:27 +0300

    lab5 nearly done

--- WindowsFormsApp2/WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -17,13 +17,20 @@ namespace WindowsFormsApp2
         public Form1()
         {
             InitializeComponent();
+            SortByType.Add("name", SortByName);
+            SortByType.Add("age", SortByAge);
+            SortByType.Add("mass", SortByMass);
+            SortByType.Add("mark", SortByMark);
         }
+        delegate void SortDel(StudentsGroup a);
+        delegate bool IsHigher(int x);
+        static Dictionary<string, SortDel> SortByType = new Dictionary<string, SortDel>();
         class Student
         {
-            string name { get; set; }
-            int age { get; set; }
-            double mass { get; set; }
-            int markFromCsharp { get; set; }
+            public string name { get; }
+            public int age { get; }
+            public double mass { get; }
+            public int markFromCsharp { get; }
             public Student()
             {
                 name = "";
@@ -31,17 +38,23 @@ namespace WindowsFormsApp2
                 mass = 0;
                 markFromCsharp = 0;
             }
-            public Student(string name,int age,double mass,int markFromCsharp) {
+            public Student(string name, int age, double mass, int markFromCsharp)
+            {
                 this.name = name;
                 this.age = age;
                 this.mass = mass;
                 this.markFromCsharp = markFromCsharp;
             }
-            public void Display()
+            public string Display(IsHigher func)
             {
-
+                if (func(markFromCsharp))
+                {
+                    return "Name: " + name + '\t' + " Age: " + age + '\t' + " Mass: " + mass + '\t' + " Mark: " + markFromCsharp + "\n";
+                }
+                return "";
             }
         }
+
         class StudentsGroup
         {
             string nameOfGroup { get; set; }
@@ -57,32 +70,113 @@ namespace WindowsFormsApp2
             }
             public static StudentsGroup operator +(StudentsGroup c1, Student c2)
             {
-                Array.Resize(ref c1.group, c1.group.Length+1);
-                c1.group[c1.group.Length-1] = c2;
-                return c1;
-            }
-            public static StudentsGroup operator -(StudentsGroup c1, Student c2)
-            {
-                Array.Resize(ref c1.group, c1.group.Length - 1);
+                Array.Resize(ref c1.group, c1.group.Length + 1);
                 c1.group[c1.group.Length - 1] = c2;
                 return c1;
             }
-            public void Display()
+            public static StudentsGroup operator -(StudentsGroup gr, Student student)
             {
-                foreach (Student i in group)
+                bool isFind = false;
+                for (int i = 0; i < gr.group.Length; i++)
                 {
-                    i.Display();
+                    if (isFind)
+                    {
+                        Student temp = gr.group[i];
+                        gr.group[i] = gr.group[i - 1];
+                        gr.group[i - 1] = temp;
+                    }
+                    if (gr.group[i] == student)
+                    {
+                        isFind = true;
+                    }
+                }
+                if (isFind)
+                {
+                    Array.Resize(ref gr.group, gr.group.Length - 1);
+                }
+                return gr;
+            }
+            public void Sort(string sortType)
+            {
+                foreach (var i in SortByType)
+                {
+                    if (i.Key == sortType.ToLower())
+                    {
+                        i.Value.Invoke(this);
+                        return;
+                    }
                 }
             }
+
+            public void Display(Label label)
+            {
+                string s = "";
+                foreach (Student i in group)
+                {
+                    s += i.Display(x => x > -1);
+                }
+                label.Text = s;
+            }
+            public void Display(int mark, Label label)
+            {
+                string s = "";
+                foreach (Student i in group)
+                {
+                    s += i.Display(x => x > mark);
+                }
+                label.Text = s;
+            }
+        }
+        void SortByName(StudentsGroup gr)
+        {
+            Array.Sort(gr.group, delegate (Student a, Student b)
+            {
+                return String.Compare(a.name, b.name);
+            });
+        }
+        void SortByAge(StudentsGroup gr)
+        {
+            Array.Sort(gr.group, delegate (Student a, Student b)
+            {
+                if (a.age < b.age) { return 1; }
+                else if (a.age > b.age) { return -1; }
+                return 0;
+            });
+        }
+        void SortByMass(StudentsGroup gr)
+        {
+            Array.Sort(gr.group, delegate (Student a, Student b)
+            {
+                if (a.mass < b.mass) { return 1; }
+                else if (a.mass > b.mass) { return -1; }
+                return 0;
+            });
+        }
+        void SortByMark(StudentsGroup gr)
+        {
+            Array.Sort(gr.group, delegate (Student a, Student b)
+            {
+                if (a.markFromCsharp < b.markFromCsharp) { return 1; }
+                else if (a.markFromCsharp > b.markFromCsharp) { return -1; }
+                return 0;
+            });
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            StudentsGroup group21=new StudentsGroup("CS-21");
-            Student student1=new Student("Petro",28,56,3);
-            Student student2=new Student("Ivan",18,57,4);
-            group21+= student1;
-            group21+= student2;
+            StudentsGroup group21 = new StudentsGroup("CS-21");
+            Student student1 = new Student("Petro", 28, 56, 3);
+            Student student2 = new Student("Ivan", 18, 57, 4);
+            Student student3 = new Student("Bohdan", 19, 50, 2);
+            Student student4 = new Student("Stepan", 20, 59, 1);
+            Student student5 = new Student("Semen", 22, 57, 1);
+            group21 += student1;
+            group21 += student2;
+            group21 += student3;
+            group21 += student4;
+            group21 -= student4;
+            group21.Sort("aGe");
+            group21.Display(0, label1);
         }
     }
 }
